bedf37101a7dfdddd0e23f446a6b58f05189ddf9
JeongHeum Byun <quswjdgma83@gmail.com>
2024-02-26 16:19:12 +0900

    기사단원

---
+++ programers/기사단원.py
@@ -0,0 +1,24 @@
+from math import sqrt
+#number: 기사단원의 수
+#limit: 협약 공격력 max
+#power: 협약 초과 무기 공격력
+
+
+def yaksu(number):
+    count = 0
+    for i in range(1, int(sqrt(number)) + 1):
+        if number % i == 0:
+            count += 1
+            if i != number // i:  # 제곱근이 아니면, 짝이 되는 약수도 존재
+                count += 1
+    return count
+
+def solution(number, limit, power):
+    answer = 0
+    for num in range(1, number+1):
+        bump = yaksu(num)
+        if bump > limit:
+            answer += power
+        else:
+            answer += bump
+    return answer
