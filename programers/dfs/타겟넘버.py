25bddc8605cc3c58df3513ff47d38eaa01035e2f
JeongHeum Byun <quswjdgma83@gmail.com>
2024-04-02 19:34:34 +0900

    타겟넘버

---
+++ programers/dfs/타겟넘버.py
@@ -0,0 +1,14 @@
+def DFS(hap, now, numbers, target):
+    if now == (len(numbers) - 1):
+        if hap == target:
+            return 1
+        return 0
+    
+    case1 = DFS(hap + numbers[now+1], now+1, numbers, target)
+    case2 = DFS(hap - numbers[now+1], now+1, numbers, target)
+    return case1 + case2
+
+def solution(numbers, target):
+    answer = 0
+    answer = DFS(numbers[0], 0, numbers, target) + DFS(-numbers[0], 0, numbers, target)
+    return answer
