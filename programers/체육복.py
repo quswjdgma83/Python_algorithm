2a917c8d90fdc512c4dea03ac02c703b86749993
JeongHeum Byun <quswjdgma83@gmail.com>
2024-04-12 23:11:41 +0900

    0412

--- programers/체육복.py
+++ programers/체육복.py
@@ -1,50 +1,18 @@
 def solution(n, lost, reserve):
-    answer = 0
-    lost = [k-1 for k in lost]
-    lost = sorted(lost)
-    reserve = [k-1 for k in reserve]
-    reserve = sorted(reserve)
-    now1 = [1 for _ in range(n)]
-    now2 = [1 for _ in range(n)]
-    for idx in range (n):
-        if idx in lost:
-            now1[idx] -= 1
-        if idx in reserve:
-            now1[idx] += 1
-            
-    for idx in range (n):
-        #앞쪽에 빌림
-        if idx > 0 and now1[idx] == 0:
-            if now1[idx-1] == 2:
-                now1[idx-1] = 1
-                now1[idx] = 1
-        #뒷쪽에 빌림
-        elif idx < n-1 and now1[idx] == 0:
-            if now1[idx+1] == 2:
-                now1[idx+1] = 1
-                now1[idx] = 1
-    answer1 = n - now1.count(0)
+    _lost = [i-1 for i in lost if i not in reserve]
+    _reserve = [i-1 for i in reserve if i not in lost]
     
-    for idx in range (n):
-        if idx in lost:
-            now2[idx] -= 1
-        if idx in reserve:
-            now2[idx] += 1
-            
-    for idx in range (n):
-        #뒷쪽에 빌림
-        if idx < n-1 and now2[idx] == 0:
-            if now2[idx+1] == 2:
-                now2[idx+1] = 1
-                now2[idx] = 1
-        #앞쪽에 빌림
-        elif idx > 0 and now2[idx] == 0:
-            if now2[idx-1] == 2:
-                now2[idx-1] = 1
-                now2[idx] = 1
-    answer2 = n - now2.count(0)
-    answer = max(answer1, answer2)
-    print(answer)
+    answer = n-len(_lost)
+    
+    target = [0 for _ in range(n)]
+    for idx in _reserve:
+        target[idx] = 1
+    _lost = sorted(_lost)
+    for l in _lost:
+        if l != 0 and target[l-1] == 1:
+            answer += 1
+            target[l-1] = 0
+        elif l != n-1 and target[l+1] == 1:
+            answer += 1
+            target[l+1] = 0
     return answer
-
-solution(5, [5, 3], [4, 2])
