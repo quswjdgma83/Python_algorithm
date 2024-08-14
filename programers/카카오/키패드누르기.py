22330000c28aa8782787be53816a670d743ed106
JeongHeum Byun <quswjdgma83@gmail.com>
2024-08-14 18:52:02 +0900

    0814

--- programers/카카오/키패드누르기.py
+++ programers/카카오/키패드누르기.py
@@ -1,47 +1,51 @@
-LH = '*'
-RH = '#'
-
-def compare(num, L, R, hand):
-    global LH, RH
-    keypad = {1: (0, 0), 2: (0, 1), 3: (0, 2),
-              4: (1, 0), 5: (1, 1), 6: (1, 2),
-              7: (2, 0), 8: (2, 1), 9: (2, 2),
-              '*': (3, 0), 0: (3, 1), '#': (3, 2)}
+def calc(f,t):
+    a = abs(f[0]-t[0])
+    b = abs(f[1]-t[1])
+    return a+b
     
-    Cind = keypad[num]
-    Lind = keypad[L]
-    Rind = keypad[R]
-    
-    Ldist = abs(Cind[0] - Lind[0]) + abs(Cind[1] - Lind[1])
-    Rdist2 = abs(Cind[0] - Rind[0]) + abs(Cind[1] - Rind[1])
-    
-    if Ldist == Rdist2:
-        if hand == "left":
-            LH = num
-            return "L"
-        else:
-            RH = num
-            return "R"
-    if Ldist < Rdist2:
-        LH = num
-        return "L"
-    else:
-        RH = num
-        return "R"
-
 def solution(numbers, hand):
-    global LH, RH
     answer = ''
-    hand = "left" if hand == "left" else "right"
-    
+    # 왼: 1, 4, 7
+    # 오: 3, 6, 9
+    # 가: 2, 5, 8, 0
+    dict_a = {
+        1: (0,0),
+        2: (0,1),
+        3: (0,2),
+        4: (1,0),
+        5: (1,1),
+        6: (1,2),
+        7: (2,0),
+        8: (2,1),
+        9: (2,2),
+        0: (3,1)
+    }
+    dict_b = {
+        "left": "L",
+        "right": "R"
+    }
+    left = (3,0)
+    right = (3,2)
     for num in numbers:
-        if num in [1, 4, 7]:
+        if num in [1,4,7]:
             answer += "L"
-            LH = num
+            left = dict_a[num]
         elif num in [3, 6, 9]:
             answer += "R"
-            RH = num
+            right = dict_a[num]
         else:
-            answer += compare(num, LH, RH, hand)
-                
+            l = calc(left, dict_a[num])
+            r = calc(right, dict_a[num])
+            if l == r:
+                answer += dict_b[hand]
+                if hand == "left":
+                    left = dict_a[num]
+                else:
+                    right = dict_a[num]
+            elif l > r:
+                answer += "R"
+                right = dict_a[num]
+            else:
+                answer += "L"
+                left = dict_a[num]
     return answer
